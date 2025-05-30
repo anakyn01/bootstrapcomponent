22c68937f878d48260abeee4c23da2d766ed6002
Your Name <you@example.com>
2025-05-30 12:21:25 +0900

    lorem

--- src/pages/Layer.jsx
+++ src/pages/Layer.jsx
@@ -36,13 +36,130 @@ data-bs-target="#one"
 </div>
         </Col>
         <Col md={3}>
-        <h1></h1>
+        <h1>Add animation</h1>
+<button
+className="btn btn-primary"
+data-bs-toggle="modal"
+data-bs-target="#any"
+>애니메이션 모달열기</button>
+
+<div className="modal fade" id="any">
+    <div className="modal-dialog">
+        <div className="modal-content">
+            <div className="modal-header">
+                <h4 className="modal-title">heading</h4>
+                <button className="btn-close"
+                data-bs-dismiss="modal"
+                ></button>
+            </div>
+            <div className="modal-body">
+                .....
+            </div>
+            <div className="modal-footer">
+                <button className="btn btn-danger"
+                data-bs-dismiss="modal"
+                >
+                close
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
         </Col>
         <Col md={3}>
-        <h1></h1>
+        <h1>fullscreen</h1>
+<button
+className="btn btn-primary"
+data-bs-toggle="modal"
+data-bs-target="#full"
+>full 모달열기</button>
+
+<div className="modal fade" id="full">
+    <div className="modal-dialog modal-fullscreen">
+        <div className="modal-content">
+            <div className="modal-header">
+                <h4 className="modal-title">heading</h4>
+                <button className="btn-close"
+                data-bs-dismiss="modal"
+                ></button>
+            </div>
+            <div className="modal-body">
+                .....
+            </div>
+            <div className="modal-footer">
+                <button className="btn btn-danger"
+                data-bs-dismiss="modal"
+                >
+                close
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
         </Col>
         <Col md={3}>
-        <h1></h1>
+        <h1>Centered Modal</h1>
+        <button
+className="btn btn-primary"
+data-bs-toggle="modal"
+data-bs-target="#center"
+>센터모달열기</button>
+
+<div className="modal fade" id="center">
+    <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+            <div className="modal-header">
+                <h4 className="modal-title">heading</h4>
+                <button className="btn-close"
+                data-bs-dismiss="modal"
+                ></button>
+            </div>
+            <div className="modal-body">
+                .....
+            </div>
+            <div className="modal-footer">
+                <button className="btn btn-danger"
+                data-bs-dismiss="modal"
+                >
+                close
+                </button>
+            </div>
+        </div>
+    </div>
+</div>
+        </Col>
+    </Row>
+    <Row>
+        <Col md={12}>
+ <h1>Centered Modal</h1>
+        <button
+className="btn btn-primary"
+data-bs-toggle="modal"
+data-bs-target="#center"
+>스크롤모달열기</button>
+
+<div className="modal fade" id="center">
+    <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div className="modal-content">
+            <div className="modal-header">
+                <h4 className="modal-title">heading</h4>
+                <button className="btn-close"
+                data-bs-dismiss="modal"
+                ></button>
+            </div>
+            <div className="modal-body">
+                .....
+            </div>
+            <div className="modal-footer">
+                <button className="btn btn-danger"
+                data-bs-dismiss="modal"
+                >
+                close
+                </button>
+            </div>
+        </div>
+    </div>
+</div>        
         </Col>
     </Row>
 </Container>
