fe89a6efc1b2fbbf15a1667b7cbd48505aa063f1
Your Name <you@example.com>
2025-05-30 12:35:36 +0900

    last

--- src/pages/Layer.jsx
+++ src/pages/Layer.jsx
@@ -135,11 +135,11 @@ data-bs-target="#center"
         <button
 className="btn btn-primary"
 data-bs-toggle="modal"
-data-bs-target="#center"
+data-bs-target="#lorem"
 >스크롤모달열기</button>
 
-<div className="modal fade" id="center">
-    <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+<div className="modal fade" id="lorem">
+    <div className="modal-dialog modal-dialog-scrollable">
         <div className="modal-content">
             <div className="modal-header">
                 <h4 className="modal-title">heading</h4>
@@ -148,7 +148,25 @@ data-bs-target="#center"
                 ></button>
             </div>
             <div className="modal-body">
-                .....
+Aliqua esse amet aliquip ipsum nostrud velit aliqua do esse. Labore amet voluptate Lorem dolor qui. In ea duis deserunt culpa laboris elit.
+
+Sunt eiusmod occaecat adipisicing labore ut. Anim dolor ea qui ullamco ut est ea occaecat esse ullamco nisi magna laboris consequat. Nisi consequat dolor aliquip magna irure. Consectetur nisi eu commodo esse elit veniam occaecat non exercitation. Nostrud voluptate excepteur enim duis eu voluptate ad quis ullamco. Elit officia minim voluptate Lorem proident fugiat quis enim reprehenderit.
+
+Dolor reprehenderit reprehenderit deserunt eu consequat aliqua elit consectetur occaecat. Enim labore irure laboris aute ad eiusmod fugiat irure adipisicing sint occaecat cillum tempor excepteur. Deserunt officia amet sunt quis veniam aute nostrud aute.
+
+Dolor duis do minim laborum non cupidatat et enim duis Lorem. Culpa pariatur proident aliqua ut veniam veniam velit commodo. Consequat sint cillum aute aute excepteur incididunt ut consequat tempor officia. Ipsum mollit ad excepteur nostrud elit amet aute.
+
+Incididunt deserunt ut quis id nulla nulla et amet consequat est id. Exercitation aliquip dolor Lorem veniam duis adipisicing adipisicing deserunt deserunt. Ea anim pariatur esse tempor consectetur tempor consectetur sit. Excepteur do sunt aute ullamco ex laboris fugiat et dolor ullamco Lorem.
+
+Pariatur labore nulla pariatur non laboris eu aliqua irure sunt ex magna ut ex ad. Officia pariatur ipsum commodo anim tempor veniam. Adipisicing anim cupidatat dolore duis et deserunt veniam commodo. Deserunt consectetur in excepteur fugiat aute non mollit eiusmod et elit reprehenderit occaecat commodo. Mollit duis aliquip sunt amet aliqua eu officia ex proident amet sunt occaecat dolor in. Dolor et sunt pariatur deserunt culpa proident fugiat officia duis amet ut qui dolore.
+
+Do consequat deserunt elit eu esse proident sunt consectetur aliquip exercitation nulla. Tempor sit fugiat pariatur cillum occaecat. Adipisicing sint aliquip eu id pariatur tempor magna est occaecat nostrud ex dolore anim. Culpa Lorem ex velit exercitation proident consectetur exercitation.
+
+Aliquip id laboris incididunt labore et fugiat nulla ea id duis veniam cupidatat aliqua. Nisi ad culpa esse id sint ex amet incididunt voluptate laboris eu adipisicing eu. Irure minim ullamco ullamco voluptate Lorem ut aute. Laborum amet enim in dolor pariatur enim sit est aute adipisicing ea. Sunt sunt duis ex dolor consectetur velit reprehenderit cupidatat non adipisicing in.
+
+Proident Lorem reprehenderit et duis ex eiusmod. Anim eu id exercitation fugiat nostrud. Magna velit commodo do fugiat eiusmod aliquip sit.
+
+Nostrud cillum fugiat pariatur nostrud quis irure aliqua sit adipisicing labore aliqua ad labore. Ut magna eu veniam ad occaecat fugiat elit qui reprehenderit non. Amet ullamco adipisicing sint ullamco tempor irure dolor do Lorem amet aliquip proident ut. Consectetur in labore excepteur aliqua sint et dolor culpa exercitation labore ipsum.
             </div>
             <div className="modal-footer">
                 <button className="btn btn-danger"
